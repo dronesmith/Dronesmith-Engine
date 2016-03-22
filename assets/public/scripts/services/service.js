91c021f6995e2a50e251a13f3497a4b38624997a
flatlevel <geoff@skyworksas.com>
2016-03-21 23:17:00 -0700

    Initial Frontend Scaffold

---
+++ assets/public/scripts/services/service.js
@@ -0,0 +1,29 @@
+'use strict';
+angular.module('myApp')
+  .service('ApiService', function ($http) {
+  this.uid = null;
+  this.aps = [
+    {
+      ssid: "test1",
+      kind: "Wi-fi"
+    },
+    {
+      ssid: "test2",
+      kind: "LTE"
+    }
+  ];
+  this.setUp = function (user,pwd) {
+    var data = JSON.stringify({
+              email: user,
+              password: pwd
+          });
+
+    $http.post("/api/setup", data).success(function(data, status) {
+            this.uid = data.id;
+            $http.get("/api/aps").success(function (data, status) {
+                this.aps = data.aps;
+            });
+       });
+  }
+
+  });
