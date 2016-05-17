9b27ef15cd485c6ae78ab2f8de0fcf920cd1ff9d
flatlevel <geoff@skyworksas.com>
2016-05-16 21:01:03 -0700

    Add cloud IP to debug script

--- debug.sh
+++ debug.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-go run -ldflags "-X config.gitHash=`git rev-parse HEAD`" src/main.go --master "/dev/tty.usbmodem1:115200" --setup "old/" --assets "$GOPATH/"
+go run -ldflags "-X config.gitHash=`git rev-parse HEAD`" src/main.go --master "/dev/tty.usbmodem1:115200" --setup "old/" --assets "$GOPATH/" --dsc "24.234.109.135:4002"
