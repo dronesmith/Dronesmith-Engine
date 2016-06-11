daff5c7e374be1619375eb61a78a7c980ed76eb5
flatlevel <geoff@skyworksas.com>
2016-06-10 19:26:22 -0700

    Move log directory to /opt/dslink/log. New edison firmwares delete the contents of /var/log/ on each power cycle

--- scripts/run.sh
+++ scripts/run.sh
@@ -7,4 +7,4 @@ set -e
 b=$(ifconfig wlan0 | perl -n -e 'if (m/inet addr:([\d\.]+)/g) { print $1 }')
 #echo $b
 
-/opt/dslink/dslink --master /dev/ttyMFD1:921600 --dsc 24.234.109.135:4002 --dscHttp 24.234.109.135:80 --status $b:80 --log /var/log/dslink/dslink_`date +%s`.log --output "0.0.0.0:14551" --flights "/opt/dslink/flights" --daemon
+/opt/dslink/dslink --master /dev/ttyMFD1:921600 --dsc 24.234.109.135:4002 --dscHttp 24.234.109.135:80 --status $b:80 --log /opt/dslink/log/dslink_`date +%s`.log --output "0.0.0.0:14551" --flights "/opt/dslink/flights" --daemon
