f7513a27685c94f538b2246113b6dbb180be3edd
flatlevel <geoff@skyworksas.com>
2016-07-25 16:00:29 -0700

    Fix fmulink

--- src/fmulink/fmulink.go
+++ src/fmulink/fmulink.go
@@ -556,17 +556,15 @@ func Serve(cl *cloudlink.CloudLink) {
               //   cl.SendSyncUnlock()
               // }
 
-              // Disable syncing
-
-              // if pv.BaseMode & 128 == 128 && !Saver.IsLogging() {
-              //   config.Log(config.LOG_INFO, "fl: Event Trigger: Start logging.")
-              //   Saver.Start()
-              //   cl.SendSyncLock(Saver.Name())
-              // } else if pv.BaseMode & 128 == 0 && Saver.IsLogging() {
-              //   config.Log(config.LOG_INFO, "fl: Event Trigger: Stop logging.")
-              //   Saver.End()
-              //   cl.SendSyncUnlock()
-              // }
+              if pv.BaseMode & 128 == 128 && !Saver.IsLogging() {
+                config.Log(config.LOG_INFO, "fl: Event Trigger: Start logging.")
+                Saver.Start()
+                cl.SendSyncLock(Saver.Name())
+              } else if pv.BaseMode & 128 == 0 && Saver.IsLogging() {
+                config.Log(config.LOG_INFO, "fl: Event Trigger: Stop logging.")
+                Saver.End()
+                cl.SendSyncUnlock()
+              }
 
               fmu.Meta.Link = FMUSTATUS_GOOD
 
